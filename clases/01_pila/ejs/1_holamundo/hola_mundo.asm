e009acb517e9cc4fdcf39505cc367e6d761c11a3
mnPanic <panicmanu@gmail.com>
2019-08-25 18:09:26 -0300

    1/1: Modifico hola mundo

--- clases/01_pila/ejs/1_holamundo/hola_mundo.asm
+++ clases/01_pila/ejs/1_holamundo/hola_mundo.asm
@@ -1,17 +1,17 @@
-section .data
-	msg: DB "Hola mundo bro", 10
-	largo EQU $ - msg
+global _start
 
-	global _start
+section .rodata
+	msg:  DB "Hola mundo!", 10, 0 	; 10: \n, 0: Fin de secuencia
+	largo EQU $ - msg
 
 section .text
 	_start:
-	mov rax, 4
-	mov rbx, 1
-	mov rcx, msg
-	mov rdx, largo
-	int 0x80
+		mov rax, 4		; 4: sys_write
+		mov rbx, 1		; 1: stdout
+		mov rcx, msg
+		mov rdx, largo
+		int 0x80
 
-	mov rax, 1
-	mov rbx, 0
-	int 0x80
+		mov rax, 1		; 1: exit
+		mov rbx, 0		; 0: ok
+		int 0x80
