554de41f90a1e5e14b5e372a5c2979761984427c
mnPanic <panicmanu@gmail.com>
2019-08-28 20:31:49 -0300

    03/3: C

--- clases/03_estructuras/clase-03_ejercicios/src/ejercicio_3/ejercicio.c
+++ clases/03_estructuras/clase-03_ejercicios/src/ejercicio_3/ejercicio.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 struct nodo{
 	int dato;
 	struct nodo *prox;
@@ -6,9 +8,26 @@ struct lista{
 	struct nodo *primero;
 };
 
-void borrarUltimo(struct lista *unaLista) {
-	// TODO
+void borrarUltimo(struct lista *l) {
+	// Voy hasta el último y lo borro
+	struct nodo** next = &(l->primero);
+	struct nodo** prev = next;
+	// Me muevo hasta que next es null, i.e llegué al último nodo,
+	// el que tengo que borrar
+	while(*next) {
+		prev = next;
+		next = &((*next)->prox);
+	}
+
+	// Borro el último nodo
+	free(*prev);
+	*prev = NULL;
 }
-void agregarPrimero(struct lista* unaLista, int unInt) {
-	// TODO
+
+void agregarPrimero(struct lista* l, int unInt) {
+	// Creo el nuevo nodo
+	struct nodo* n = malloc(sizeof(struct nodo));
+	n->dato = unInt;
+	n -> prox = l->primero;
+	l->primero = n;
 }
